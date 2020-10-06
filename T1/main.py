5fd5184706fdcd4546ad5454d41097e3742c3b50
Ariel Suil <ariel.suil@ug.uchile.cl>
2020-10-06 17:21:20 -0300

    added data separation for train/test; added tests; added performace calculations and display

--- T1/main.py
+++ T1/main.py
@@ -65,12 +65,11 @@ CLEANED_OUTPUTS = [[1, 0] if value == 'g' else [0, 1] for value in ALL_OUTPUTS]
 
 
 # SEPARE TRAIN AND TEST DATA
-"""
-===============
-IMPORTANT TO-DO
-===============
-"""
-
+# for now we'll use 50% as train data and 50% as test data
+# luckily our dataset intercalates g and b results, so we can ensure 50/50 data and reduce bias
+split_index = int(len(CLEANED_INPUTS)*0.5)
+X_TRAIN, X_TEST = CLEANED_INPUTS[:split_index], CLEANED_INPUTS[split_index:]
+Y_TRAIN, Y_TEST = CLEANED_OUTPUTS[:split_index], CLEANED_OUTPUTS[split_index:]
 
 # START SETUP FOR THE MODEL
 
@@ -78,10 +77,10 @@ IMPORTANT TO-DO
 np.random.seed(42)
 
 # The prepared input data
-X = np.array(CLEANED_INPUTS).transpose()
+X = np.array(X_TRAIN).transpose()
 
 # The outputs for all input data
-Y = np.array(CLEANED_OUTPUTS).transpose()
+Y = np.array(Y_TRAIN).transpose()
 
 # Set the hyperparameters
 n_x = X.shape[0]    # No. of neurons in first layer (33 inputs)
@@ -89,7 +88,7 @@ n_h = 17            # No. of neurons in hidden layer (17 felt right)
 n_y = Y.shape[0]    # No. of neurons in output layer (2 outputs)
 
 # The number of times the model has to learn the dataset
-number_of_iterations = 1000
+number_of_iterations = 10000
 learning_rate = 0.01
 
 # define a model
@@ -97,26 +96,46 @@ trained_parameters = model(X, Y, n_x, n_h, n_y, number_of_iterations, learning_r
 
 
 # RUN TESTS AND RECORD PERFORMANCE
-"""
-===============
-IMPORTANT TO-DO
-===============
-"""
+results = []
+for i in range(len(X_TEST)):
+    test_row = X_TEST[i]
+    x_test = np.array([[n] for n in test_row])
+    y_predict = predict(x_test, trained_parameters)
+    results += [y_predict]
 
+# TURN RESULT DATA INTO READABLE RESULTS
+true_g = 0  # pred G, actual G
+false_g = 0  # pred G, actual B
+true_b = 0  # pred B, actual B
+false_b = 0  # pred B, actual G
+not_determined = 0  # output [0, 0] or [1, 1]
+actual_g = sum([out[0] for out in Y_TEST])  # number of real gs
+actual_b = sum([out[1] for out in Y_TEST])  # number of real bs
 
-# DISPLAY PERFORMANCE RESULTS
-"""
-===============
-IMPORTANT TO-DO
-===============
-"""
+for i in range(len(results)):
+    if results[i] == Y_TEST[i]:  # true
+        if results[i] == [1, 0]:  # g
+            true_g += 1
+        else:  # b
+            true_b += 1
+    elif results[i][0] == results[i][1]:  # [0, 0] or [1, 1] => not determined
+        not_determined += 1
+    else:  # false
+        if results[i] == [1, 0]:  # g
+            false_g += 1
+        else:  # b
+            false_b += 1
 
-
-# simple temporary starter test
-# (run on training data, just to check parameters do work)
-test_row = CLEANED_INPUTS[42]
-X_test = np.array([[n] for n in test_row])
-y_predict = predict(X_test, trained_parameters)
-
-# Print the result
-print(f"Expected: {CLEANED_OUTPUTS[42]}\nGot: {y_predict}")
+# DISPLAY RESULTS
+print(f"           | pred 'g' \t| pred 'b' \t| total")
+print(f"actual 'g' | {true_g} \t\t| {false_b} \t\t| {actual_g}")
+print(f"actual 'b' | {false_g} \t\t\t| {true_b} \t\t| {actual_b}")
+print(f"not determined: {not_determined}\n")
+prec_g = true_g / (true_g + false_g)
+reca_g = true_g / actual_g
+prec_b = true_b / (true_b + false_b)
+reca_b = true_b / actual_b
+print(f"Results for 'g' class")
+print(f"precision: {str(prec_g)[:6]}\trecall: {str(reca_g)[:6]}")
+print(f"Results for 'b' class")
+print(f"precision: {str(prec_b)[:6]}\trecall: {str(reca_b)[:6]}")
