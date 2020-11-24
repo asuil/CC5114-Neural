325eb8ef94b918dd48d5c9cb51bba14ddf7877ed
Ariel Suil <ariel.suil@ug.uchile.cl>
2020-11-24 15:28:08 -0300

    finished N-Queen Problem

--- T2/main.py
+++ T2/main.py
@@ -27,10 +27,18 @@ from genetic_algorithm import GA
 import matplotlib.pyplot as mpl
 import random
 
-N = 4
-random.seed(43)
+# dimensions of chess board and number of queens
+N = 30
+# model parameters
+random.seed(10)
+MUTATION_RATE = 0.02
+POPULATION_SIZE = 700
+GENERATIONS = 80
 
 
+# checks if 2 queens are attacking each other
+# 1 if attacking, 0 else
+# list(int, int), list(int, int) => int
 def is_attacking(q1, q2):
     # same x
     if q1[0] == q2[0]:
@@ -48,13 +56,16 @@ def is_attacking(q1, q2):
     return 0
 
 
+# calculates how many queens are atacking each other and turns into fitness value
+# list(int) -> int
 def fitness(individual):
 
+    # separate queens
     queens = [None] * N
-    for i in range(N):
-        queen = individual[2*i:2*(i+1)]
-        queens[i] = queen
+    for j in range(N):
+        queens[j] = individual[2*j:2*(j+1)]
 
+    # calculate # of attacks
     attacks = 0
     indices = range(N)
     for this_queen in indices:
@@ -64,31 +75,40 @@ def fitness(individual):
             if this_queen != other_queen:
                 attacks += is_attacking(queens[this_queen], queens[other_queen])
 
+    # return fitness value
     return - attacks / 2
 
 
+# generates a valid gene
+# None -> int
 def gene_factory():
     return random.randint(1, N)
 
 
+# generates a valid individual
+# None -> array(int)
 def individual_factory():
     return [gene_factory() for i in range(N * 2)]
 
 
-ga = GA(pop_size=100, mutation_rate=0.2, fitness=fitness,
+# build algorithm
+ga = GA(pop_size=POPULATION_SIZE, mutation_rate=MUTATION_RATE, fitness=fitness,
         individual_factory=individual_factory, gene_factory=gene_factory,
-        termination_condition=lambda f: f == 0, silent=False, max_iter=10)
+        termination_condition=lambda f: f == 0, silent=False, max_iter=GENERATIONS)
 
+# run algorithm
 best_fitness_list, avg_list, best_individual = ga.run()
 
-print(best_individual)
+# display results
 ga.graph()
 
+# separate queens on best individual
 queen_list = [None] * N
 for i in range(N):
     q = best_individual[2*i:2*(i+1)]
     queen_list[i] = q
 
+# plot queens to visualize solution
 for queen in queen_list:
     mpl.plot(queen[0], queen[1], marker='o')
 mpl.show()
